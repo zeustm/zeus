4f8ded2d3b1861895463c2f0fc2024176bace794
Shahab <safaeishahab@yahoo.com>
2016-01-21 17:14:23 +0330

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,27 @@
+do
+
+function run(msg, matches)
+  return 'Umbrella Telegram Bot v1.5'.. [[ 
+  
+  Weblog:
+  http://HellBot.Blogfa.Com
+  Antispam Bot: @HellAntiSpam
+  Channel: @HellBotCh
+  Sudo (Admin): @Shahabambesik
+  
+  Powered by:
+  Shahabsaf on Teleseed
+  Engineer Shahabsaf
+  http://HellBot.Blogfa.Com]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
