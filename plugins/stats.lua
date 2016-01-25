7269170711fa2f50c224166d989f7c6acc963369
Shahab <safaeishahab@yahoo.com>
2016-01-25 18:23:39 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (zeus)",-- Put everything you like :)
+		"^[!/]([Zz]eus)"-- Put everything you like :)
     }, 
   run = run
 }
